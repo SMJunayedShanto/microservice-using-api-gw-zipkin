8eea8148f316be248af56675eec6721d3c602b78
S M Junayed Shanto <smjunayedshanto@gmail.com>
2023-10-19 00:21:43 +0600

    discovery, congif server & api gateway added

---
+++ student/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+-- INSERT Query 1
+INSERT INTO student (id, school_id, email, first_name, last_name)
+VALUES (1, 101, 'john.doe@example.com', 'John', 'Doe');
+
+-- INSERT Query 2
+INSERT INTO student (id, school_id, email, first_name, last_name)
+VALUES (2, 102, 'jane.smith@example.com', 'Jane', 'Smith');
+
+-- INSERT Query 3
+INSERT INTO student (id, school_id, email, first_name, last_name)
+VALUES (3, 101, 'bob.jones@example.com', 'Bob', 'Jones');
+
+-- INSERT Query 4
+INSERT INTO student (id, school_id, email, first_name, last_name)
+VALUES (4, 103, 'susan.wilson@example.com', 'Susan', 'Wilson');
+
+-- INSERT Query 5
+INSERT INTO student (id, school_id, email, first_name, last_name)
+VALUES (5, 102, 'michael.brown@example.com', 'Michael', 'Brown');

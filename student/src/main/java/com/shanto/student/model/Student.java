571e4fec1303595773bd18014c06ba396b2dc541
S M Junayed Shanto <sm.shanto@blmanagedservices.com>
2023-10-18 21:32:12 +0600

    first commit

---
+++ student/src/main/java/com/shanto/student/model/Student.java
@@ -0,0 +1,23 @@
+package com.shanto.student.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String schoolId;
+}
---
+++ student/src/main/java/com/shanto/student/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.shanto.student.repository;
+
+import com.shanto.student.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    List<Student> findAllBySchoolId(Integer schoolId);
+
+}
